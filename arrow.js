15b57a2be4615086806214080aaaa2d2e6efdc47
Domenic <domenicdrw@gmail.com>
2023-03-10 22:18:08 -0500

    added thing

---
+++ arrow.js
@@ -0,0 +1,9 @@
+function squareAndFindEvens(numbers){
+    let squares = numbers.map((num) =>{
+      return num ** 2;
+    });
+    let evens = squares.filter((square) =>{
+      return square % 2 === 0;
+    });
+    return evens;
+  }
